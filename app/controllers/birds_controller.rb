e5632298cc35f3dc44251d438766ec33ca2ab20a
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-09-28 14:40:02 +0300

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -8,9 +9,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
+
 
   # GET /birds/:id
   def show
@@ -21,5 +23,12 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 
 end
